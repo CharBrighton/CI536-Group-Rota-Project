e25b7c3d9bb5fe66bf4cf820bf09fbb6b9e987ef
foy3110 <74569280+foy3110@users.noreply.github.com>
2025-04-23 12:33:55 +0100

    added in go to manager

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Start the session
-session_start();
-// Destroy the active session
-session_destroy();
-// Redirect to the login pag
-header('Location: index.php');
-exit;
-?>
---
+++ pages/logout.php
@@ -0,0 +1,9 @@
+<?php
+// Start the session
+session_start();
+// Destroy the active session
+session_destroy();
+// Redirect to the login pag-
+header('Location: index.php');
+exit;
+?>
