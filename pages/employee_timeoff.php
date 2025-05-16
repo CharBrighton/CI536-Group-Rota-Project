1eb2734ebbeded4b6b3291d2a2839ef48809ffe5
vipxr <vipxrampagexx99@gmail.com>
2025-05-16 09:56:08 +0100

    employee_timeoff.php linked to correct logic file

--- pages/employee_timeoff.php
+++ pages/employee_timeoff.php
@@ -12,6 +12,74 @@ if (!isset($_SESSION['account_loggedin'])) {
 if (isset($_SESSION['manager'])) {
     header("location:manager_index.php");
 }
+
+class Employee // PHP class for handling page
+{
+
+    private int $id;
+
+    public function __construct() // Class constructor: sets user ID
+    {
+        $this->setId($_SESSION['account_id']);
+    }
+
+    public function setId($id): void  // ID setter, prevents dealing with globals
+    {
+        $this->id = $id;
+    }
+
+
+    private function statusValues($status): array  // returning values in array based on status result
+    {
+        switch ($status) {
+            case 0:
+                return ["pending-row", "Pending"];
+            case 1:
+                return ["accepted-row", "Accepted"];
+            case -1:
+                return ["declined-row", "Declined"];
+            default:
+                return ["Unexpected Value: " . $status, "Unexpected Value: " . $status];
+        }
+    }
+
+
+    public function populateRequests($current): string  // Populating current and previous requests based on variable
+    {
+        global $conn;
+
+        // Gets all requests
+        $result = mysqli_query($conn, "SELECT * FROM `timeoff_requests`");
+        $output = "";
+
+        while ($row = mysqli_fetch_assoc($result)) {  // Iterating through requests
+            $status = $row['request_status'];
+            $requested_date = $row['shift_date'];
+            $statusValues = $this->statusValues($status);  // Getting status class and text values from status code
+
+            if ($this->id == $row["employee_id"] && $status != -2) {  // If current user & not cancelled
+
+                if (($current && (date("Y-m-d") < $requested_date)) ||  // Current: after today
+                    (!$current) && (date("Y-m-d") >= $requested_date)) {  // Previous: today and before
+                    $output .= '<div class="request-div ' . $statusValues[0] . '">';
+
+                    // only adds checkboxes to current request rows
+                    if ($current) {
+                        $output .= '<input value="' . $requested_date . '" name="cancel-checkbox" type="radio" form="requestForm">';
+                    }
+                    $output .= "<p> " . $requested_date . " </p>";
+                    $output .= "<p> " . $row['request_date'] . " </p>";
+                    $output .= "<p> " . $statusValues[1] . " </p>";
+                    $output .= '</div>';
+                }
+            }
+        }
+
+        return $output;
+    }
+}
+
+$employee = new Employee();
 ?>
 
 <!DOCTYPE html>
@@ -22,13 +90,15 @@ if (isset($_SESSION['manager'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../js/employee_timeoff.js"></script>
+    <script src="../js/timeoff.js"></script>
     <link href="../css/general_css.css" rel="stylesheet">
+    <link href="../css/timeoff.css" rel="stylesheet">
 </head>
 
 <body>
 <div class="wrapper">
 
+    <!-- Navbar -->
     <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="employee_index.php">Logo (Employee Portal)</a>
@@ -43,7 +113,6 @@ if (isset($_SESSION['manager'])) {
                     <li class="nav-item">
                         <a class="nav-link" href="employee_timeoff.php">Time Off</a>
                     </li>
-                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="employee_dayavailability.php">Day Availability</a>
                     </li>
@@ -54,14 +123,25 @@ if (isset($_SESSION['manager'])) {
             </div>
         </div>
     </nav>
+
+
     <div id="content">
+        <!-- Current Requests -->
         <div class="container" id="current-requests">
             <p>Current Requests</p>
             <div class="scroll-area" id="current-scroll">
+                <div class="request-div">
+                    <p></p>
+                    <p> Requested Date </p>
+                    <p> Date of Request </p>
+                    <p> Request Status </p>
+                </div>
+                <?php echo $employee->populateRequests(true); ?>
             </div>
 
+            <!-- Current Requests Buttons -->
             <div class="submit_row">
-                <form action="../logic/employee_timeoff_logic.php" method="post" id="requestForm">
+                <form action="../logic/timeoff_logic.php" method="post" id="requestForm">
                     <label> Date requested:
                         <input type="date" id="inpAddRequest" name="requestedDate">
                     </label>
@@ -71,13 +151,22 @@ if (isset($_SESSION['manager'])) {
             </div>
         </div>
 
-        <br><br>
+        <div class="row-break"></div>
 
+        <!-- Previous Requests -->
         <div class="container" id="previous-requests">
             <p>Previous Requests</p>
             <div class="scroll-area" id="previous-scroll">
+                <div class="request-div">
+                    <p> Requested Date </p>
+                    <p> Date of Request </p>
+                    <p> Request Status </p>
+                </div>
+                <?php echo $employee->populateRequests(false); ?>
             </div>
+
         </div>
+        <div class="row-break"></div>
     </div>
 
 
